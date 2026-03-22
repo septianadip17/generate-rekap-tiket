8456134eaa8de3a0881d849314ffb92105f100da
iay kane <septianadip7998@gmail.com>
2026-03-22 14:25:01 +0700

    feat: image logo pln

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 export default function Navbar() {
   return (
     <div className="w-full bg-gray-900 border-b border-gray-800 px-4 md:px-10 py-3 flex justify-between items-center">
-      <h1 className="font-bold text-lg md:text-xl">
+      <h1 className="font-bold text-lg md:text-xl text-white">
         MSRS Ticket Generator
       </h1>
 
       <span className="text-xs text-gray-400">
-        Internal Tools
+        <img src="../../public/pln-icon-plus-logo.png" alt="pln icon plus logo" className="h-24 w-28 object-cover" />
       </span>
     </div>
   );
