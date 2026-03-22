b4d09e1e2f819d1c973f04229f87e5110e6ef9e7
iay kane <septianadip7998@gmail.com>
2026-03-22 14:27:09 +0700

    fix: guide text

--- src/components/Guide.jsx
+++ src/components/Guide.jsx
@@ -6,15 +6,15 @@ export default function Guide() {
       <pre className="text-xs text-gray-300 whitespace-pre-wrap">
 {`Berikut kami sampaikan informasi terkait alarm proaktif:
 Deskripsi Alarm:
-SID               : 02000190005
-NAMA PELANGGAN    : BALI-DISKOMINFO.BEDUGUL
-IP                : 172.26.150.34
-ALARM START       : 2026-03-07 09:43:22
-ALARM             : UNAVAILABLE BY ICMP PING
-STATUS PENGECEKAN : DOWN
-INDIKASI GANGGUAN : LOST POWER DI SISI UPE/CPE/PELANGGAN
-ID TIKET          : A0478423
-WAKTU TIKET DIBUAT: 07/03/2026 15:07:07`}
+SID                 : 02000190005
+NAMA PELANGGAN      : BALI-DISKOMINFO.BEDUGUL
+IP                  : 172.26.150.34
+ALARM START         : 2026-03-07 09:43:22
+ALARM               : UNAVAILABLE BY ICMP PING
+STATUS PENGECEKAN   : DOWN
+INDIKASI GANGGUAN   : LOST POWER DI SISI UPE/CPE/PELANGGAN
+ID TIKET            : A0478423
+WAKTU TIKET DIBUAT  : 07/03/2026 15:07:07`}
       </pre>
 
       <p className="text-xs text-gray-500 mt-2">
